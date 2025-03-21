0bc794174eccbe408acb88a75e10613cbbebe84f
Felipe Barbosa <felipebcm79@gmail.com>
2025-03-21 03:25:16 +0000

    código finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -40,13 +40,13 @@ int main (){
     scanf("%d" , &pontos_turisticos);
 
     //Exibindo ao usuário os dados da primeira carta, um por um, por linha.
-    printf("Carta 1: ");
+    printf("Carta 1: \n");
     printf("Estado: %s\n" , estado);         
     printf("Código: %s\n" , codigo);
     printf("Cidade: %s\n" , cidade);
     printf("População: %d\n" , populacao);
-    printf("Área: %1.f km²\n" , area);
-    printf("PIB: %1.f\n" , pib);
+    printf("Área: %f km²\n" , area);
+    printf("PIB: %f\n" , pib);
     printf("Pontos turísticos: %d\n" , pontos_turisticos);
 
     //Criando as variáveis que são solicitadas na segunda carta.
@@ -59,7 +59,8 @@ int main (){
     int pontos_turisticos2;
 
     //Pedindo ao usuário os dados que fazem parte da segunda carta com uma mensagem diferente para cada um, solicitando as informações.
-    printf("Agora digite os valores da sua segunda carta em seguida: ");
+    printf("Agora digite os valores da sua segunda carta em seguida: \n");
+    
     printf("Digite o Estado da sua segunda carta: ");
     scanf("%s" , &estado2); 
 
@@ -82,13 +83,13 @@ int main (){
     scanf("%d" , &pontos_turisticos2);
 
     //Exibindo ao usuário os dados da segunda carta, um por um, por linha.
-    printf("Carta 1: ");
+    printf("Carta 2: \n");
     printf("Estado: %s\n" , estado2);         
     printf("Código: %s\n" , codigo2);
     printf("Cidade: %s\n" , cidade2);
     printf("População: %d\n" , populacao2);
-    printf("Área: %1.f km²\n" , area2);
-    printf("PIB: %1.f\n" , pib2);
+    printf("Área: %f km²\n" , area2);
+    printf("PIB: %f\n" , pib2);
     printf("Pontos turísticos: %d\n" , pontos_turisticos2);
 
     return 0;
